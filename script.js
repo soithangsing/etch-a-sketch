0220f5c8517845be2ba4150c4abb79cc8dc36090
soithangsing <getdatasoi@outlook.com>
2022-02-01 12:47:40 +0530

    Added ability to sketch

---
+++ script.js
@@ -0,0 +1,22 @@
+const gridContainerElement = document.querySelector('.grid__container');
+
+
+for(let i = 0; i < 16 * 16; i++) {
+    let div = document.createElement('div');
+    div.classList.add('grid__items')
+    gridContainerElement.appendChild(div);
+}
+
+const gridItems = document.querySelectorAll('.grid__items');
+
+// Random Color Function 
+
+const randomInt = (min,max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () => `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`;
+
+gridItems.forEach(item => {
+    item.addEventListener('mouseenter', function(e) {
+        item.style.backgroundColor = randomColor();
+    })
+})
