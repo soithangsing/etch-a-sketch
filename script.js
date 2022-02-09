c5cb1b6e38a2e954cb62b6b3bfddc5d764c64600
soithangsing <getdatasoi@outlook.com>
2022-02-09 12:53:10 +0530

    removed evoley font, added functionality for slide change...big thanks to michalosman code, need to refactor code and add eraser and color brushes and make it work on mouse down and mouse over not mouse enter

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
+'use strict'
+
+let currentSize = 16;
+
 const gridContainerElement = document.querySelector('.grid__container');
 const eraseButton = document.getElementById('eraser-button');
+const slider = document.getElementById('myRange');
+const sliderValue = document.querySelector('.size__value')
 
-for(let i = 0; i < 16 * 16; i++) {
-    let div = document.createElement('div');
-    div.classList.add('grid__items')
-    gridContainerElement.appendChild(div);
-}
+// *********Create Grid Function start 
+
+function createGrid(size) {
+    gridContainerElement.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainerElement.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    for(let i =0; i < size * size; i++) {
+        let div = document.createElement('div');
+                div.classList.add('grid__items')
+                gridContainerElement.appendChild(div);
+                div.addEventListener('mouseenter', function(e) {
+                    e.target.style.backgroundColor = randomColor();
+                })
+            }
+        }
+        
+
+// *********Create Grid Function end
+        
+// *********Random Color Function 
+    
+const randomInt = (min,max) => Math.floor(Math.random() * (max - min + 1) + min);
+    
+const randomColor = () => `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`;
+
+// *********Random Color Function end
+
+createGrid(currentSize);
 
 const gridItems = document.querySelectorAll('.grid__items');
 
-// Random Color Function 
-
-const randomInt = (min,max) => Math.floor(Math.random() * (max - min + 1) + min);
-
-const randomColor = () => `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`;
+// Function for sketching
 
 gridItems.forEach(item => {
     item.addEventListener('mouseenter', function(e) {
@@ -22,7 +47,28 @@ gridItems.forEach(item => {
 })
 
 eraseButton.addEventListener('click', function(e) {
-    gridItems.forEach(item => {
-            item.style.backgroundColor = '#fff';
-    })
+    gridContainerElement.innerHTML = ``;
+    createGrid(currentSize);
 })
+// Function for sketching end
+
+
+slider.onmousemove = (e) => updateSizeValue(e.target.value);
+slider.onchange = (e) => {
+    currentSize = e.target.value;
+    gridContainerElement.innerHTML = ``;
+    createGrid(currentSize);
+}
+
+function updateSizeValue (value) {
+    sliderValue.innerHTML = `${value} x ${value}`;
+}
+
+function setSize (newSize) {
+    currentSize = newSize;
+}
+
+function sizeChange(value) {
+    setSize(value);
+    reloadGrid();
+}
